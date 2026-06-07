58c150c52d69f5d0719ce9c3ef28645389d29672
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-06-07 19:26:18 +0800

    Feature Build: Connected the components for the About Us on the route

--- src/app/pages/AboutUs.tsx
+++ src/app/pages/AboutUs.tsx
@@ -1,5 +1,22 @@
+import { AboutUsProvider } from "../context/aboutUsContext"
+import Hero from "../sections/about-us/Hero"
+import About from "../sections/about-us/About"
+import Philosophy from "../sections/about-us/Philosophy"
+import VisionMission from "../sections/about-us/VisionMission"
+import QualityPolicy from "../sections/about-us/QualityPolicy"
+import Team from "../sections/about-us/Team"
+
 export default function AboutUs() {
   return (
-    <></>
+    <AboutUsProvider>
+      <main>
+        <Hero></Hero>
+        <About></About>
+        <Philosophy></Philosophy>
+        <VisionMission></VisionMission>
+        <QualityPolicy></QualityPolicy>
+        <Team></Team>
+      </main>
+    </AboutUsProvider>
   )
 }
--- src/app/types/AboutUsType.ts
+++ src/app/types/AboutUsType.ts
@@ -1,1 +1,38 @@
-export interface AboutUsType {}
+interface PhilosophyList {
+  PhilosophyTitle: string,
+  PhilosophyContent: string
+}
+
+interface VissionList {
+  VissionTitle: string,
+  VissionContent: string
+}
+
+interface QualityPolicyInfo {
+  QualityPolicyTitle: string,
+  QualityPolicyContent: string,
+}
+
+interface TeamInfo {
+  PersonName: string,
+  PersonRole: string,
+  PersonImage: string,
+}
+
+export interface AboutUsType {
+  HeroInfo: string,
+  AboutUsInfo: string,
+  PhilosophyInfo: {
+    Purpose: string,
+    PhilosophyList: PhilosophyList[]
+  },
+  VissionAndMissionInfo: {
+    Vission: {
+      VissionInfo: string,
+      VissionList: VissionList[]
+    },
+    MissionInfo: string
+  },
+  QualityPolicyInfo: QualityPolicyInfo[],
+  TeamInfo: TeamInfo[]
+}
