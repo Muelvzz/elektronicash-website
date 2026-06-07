ef343f98ddf9d1647708fd88cbdcaf0a708c8c6e
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-06-07 19:39:43 +0800

    Feature Build: Developed the About Us Section of the About Us Page

--- src/app/sections/about-us/About.tsx
+++ src/app/sections/about-us/About.tsx
@@ -1,5 +1,21 @@
+import { useAboutUsContext } from "@/app/context/aboutUsContext"
+
 export default function About() {
-  return(
-    <></>
+  const { AboutUsInfo } = useAboutUsContext()
+
+  return (
+    <article className="h-screen flex flex-col lg:flex-row items-center lg:mx-30">
+      <div className="flex items-center flex-col lg:flex-row-reverse gap-y-5">
+        <div className="flex justify-center lg:flex-1">
+          <img src="/logo.png" alt="About Us Image" />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-y-5 lg:flex-1">
+          <div>
+            <h1 className="font-heading text-center">About Us</h1>
+            <p className="text-justify indent-10">{ AboutUsInfo }</p>
+          </div>
+        </div>
+      </div>
+    </article>
   )
 }
--- src/app/sections/about-us/Hero.tsx
+++ src/app/sections/about-us/Hero.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
 
   return (
     <article className="bg-(--secondary-color) w-full h-screen flex flex-col justify-center items-center text-white">
-      <div>
+      <div className="lg:w-1/2">
         <h2 className="font-heading text-center">{ HeroInfo }</h2>
       </div>
     </article>
